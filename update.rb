628db8c628aa99adec3d5f81d59166eff47c25d6
Feng Liu <liufeng@cnliufeng.com>
2012-05-14 09:45:51 -0500

    Now finished save tweets to db.

--- update.rb
+++ update.rb
@@ -1,9 +1,18 @@
 #!/usr/bin/env ruby
 
 USERNAME = 'liufeng'
+DATABASE = 'data.db'
 
 require 'twitter'
+require 'sequel'
 
-Twitter.user_timeline(USERNAME, :count => 20).each do |tweet|
-  puts "#{tweet.id} #{tweet.created_at} #{tweet.text}"
+DB = Sequel.sqlite(DATABASE)
+
+db = DB[:tweets]
+
+Twitter.user_timeline(USERNAME, :count => 200).reverse.each do |tweet|
+  if not db.filter(:id => tweet.id).first
+    db.insert(:id => tweet.id, :created_at => tweet.created_at, :text => tweet.text)
+    puts "#{tweet.created_at}"
+  end
 end
